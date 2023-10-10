0f22a5492aee72481e8290fc4bbe479598e128da
drbrgo <d.b.goga@gmail.com>
2023-10-09 20:39:13 -0400

    finished studio spa day pt3, now for bonus missions

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,10 +1,20 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
+    @NotBlank(message = "Username is required!")
+    @Size(min=5, max=15, message = "Username must be 5-15 characters in length.")
     private String username;
+    @Email(message = "Email format is mandatory!")
     private String email;
+    @NotBlank(message = "Password is required!")
+    @Size(min=6, message = "Password must be at least 6 characters in length!")
     private String password;
 
+    //private String verify?
     public User() {
 
     }
